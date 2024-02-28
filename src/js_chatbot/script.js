e4be6ad4a04077dcdfcfc92a6f3962810e5e9bac
michaelwfc <wang_feicheng@163.com>
2024-02-28 21:58:03 +0800

    update launch

--- src/js_chatbot/script.js
+++ src/js_chatbot/script.js
@@ -6,7 +6,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "sk-***"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
